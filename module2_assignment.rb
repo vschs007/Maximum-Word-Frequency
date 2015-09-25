2c76bb91e56972564d2138afbfaf4e55d60bc0f6
Manuel Lorenzo <manulorenzop@gmail.com>
2015-09-25 16:03:17 +0200

    started working on the calculate_word_frequency method

--- module2_assignment.rb
+++ module2_assignment.rb
@@ -7,6 +7,7 @@ class LineAnalyzer
   #* highest_wf_words - an array of words with the maximum number of occurrences (calculated)
   #* content,         - the string analyzed (provided)
   #* line_number      - the line number analyzed (provided)
+  attr_accessor :highest_wf_count, :highest_wf_words, :content, :line_number
 
   #Add the following methods in the LineAnalyzer class.
   #* initialize() - taking a line of text (content) and a line number
@@ -16,12 +17,26 @@ class LineAnalyzer
   #* take in a line of text and line number
   #* initialize the content and line_number attributes
   #* call the calculate_word_frequency() method.
-
+  def initialize(content, line)
+    self.content = content
+    self.line = line
+  end
   #Implement the calculate_word_frequency() method to:
   #* calculate the maximum number of times a single word appears within
   #  provided content and store that in the highest_wf_count attribute.
   #* identify the words that were used the maximum number of times and
   #  store that in the highest_wf_words attribute.
+  def calculate_word_frequency()
+    h = Hash.new
+    words = @content.split
+    words.each { |w|
+      if h.has_key?(w)
+        h[w] += 1
+      else
+        h[w] = 1
+      end
+    }
+  end
 end
 
 #  Implement a class called Solution.
@@ -30,7 +45,7 @@ class Solution
   # Implement the following read-only attributes in the Solution class.
   #* highest_count_across_lines - a number with the value of the highest frequency of a word
   #* highest_count_words_across_lines - an array with the words with the highest frequency
-
+attr_reader :highest_count_across_lines, :highest_count_words_across_lines
   # Implement the following methods in the Solution class.
   #* analyze_file() - processes 'test.txt' intro an array of LineAnalyzers
   #* calculate_line_with_highest_frequency() - determines which line of
